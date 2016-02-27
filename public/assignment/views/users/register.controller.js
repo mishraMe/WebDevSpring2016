bba13b9fa673a5c3463cfa25e6f6548e16b3c067
mishrame <mishrame@husky.neu.edu>
2016-02-27 01:31:11 -0500

    changed the correct path in the index file for the assignment link

--- public/assignment/views/users/register.controller.js
+++ public/assignment/views/users/register.controller.js
@@ -5,9 +5,6 @@
 
 
     function registerController($scope, $location, $rootScope, UserService) {
-        var user = this.user;
-        $scope.register = register;
-
         function register() {
             console.log("register controller register event on click");
             this.$location.path('/profile');

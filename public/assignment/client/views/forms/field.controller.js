f38d55985ef08e36cafdf8e33b22b2acbfb84c18
mishrame <mishrame@husky.neu.edu>
2016-03-20 07:45:18 -0400

    added field view working, yet non of the functionalities working yet.

--- public/assignment/client/views/forms/field.controller.js
+++ public/assignment/client/views/forms/field.controller.js
@@ -8,9 +8,6 @@
 
 
         var vm = this;
-
-        //vm.addForm = addForm;
-
         function init(){
             vm.currentForm = FormService.getCurrentForm();
             console.log("currentForm value is");
@@ -20,30 +17,10 @@
                 .getFieldsForForm(vm.currentForm._id)
                 .then(function(response){
                     console.log("response data is passed from model YAY");
-                    console.log(response.data);
+                   vm.fields=(response.data);
                 });
         }
         init();
-
-        //function addField(fieldType){
-        //
-        //    var newField = {
-        //        label:
-        //    }
-        //
-        //
-        //    FormService
-        //        .createFieldInForm(vm.currentForm._id, )
-        //        .then(function(response){
-        //            FormService
-        //                .findAllFormsForUser(vm.currentUser._id)
-        //                .then(function(resp){
-        //                    vm.forms= resp.data;
-        //                });
-        //        });
-        //    vm.form = null;
-        //}
-
 
     }
 

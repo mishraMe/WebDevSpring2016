cb36b8e264e85f72fb907bf3d40d4407ff6bdff5
mishrame <mishrame@husky.neu.edu>
2016-03-30 23:46:32 -0400

    added mongoose and cookie parser and sessions and other installs

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 var app = express();
 var bodyParser    = require('body-parser');
 var multer        = require('multer');
+//adding session and mongoose
+var passport = require('passport');
+
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var mongoose      = require('mongoose');
+
+mongoose.connect('mongodb://localhost/formMaker');
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 //app.get('/hello', function(req, res){
 //    res.send('hello world');
 //});
@@ -11,6 +21,14 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 require("./public/assignment/server/app.js")(app);
 require("./public/project/server/app.js")(app);
---
+++ server2.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cs5610');
+
+var courseSchema = new mongoose.Schema({
+
+    title: String,
+    seats: {type: Number, default: 50},
+    starts:{type: Date, default: Date.now}
+}, {collection: "course"});
+
+var course = mongoose.model("Course", courseSchema);
+
+course
+    .create({title: "MongoDB"},
+         function(err, results){
+    console.log(err);
+    console.log(results);
+})
+
+function findAll(callback){
+    course.find(callback);
+}
+app.get('/rest/course', function(req, res) {
+    findAll(function(err, results){
+        res.json(results);
+    })
+});
+
+app.listen(3000);

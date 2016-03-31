147a2f7c2c1e3bd77a7ed45c4aa70c2e5e102dd5
mishrame <mishrame@husky.neu.edu>
2016-03-31 15:16:27 -0400

    added user model

--- public/assignment/server/models/user.model.server.js
+++ public/assignment/server/models/user.model.server.js
@@ -1,6 +1,12 @@
-var mockUsers = require("./user.mock.json");
 
-module.exports = function(app) {
+module.exports = function(app, db , mongoose) {
+
+
+    var UserSchema = require('./user.schema.server.js')(mongoose);
+
+    // create user model from schema
+    var UserModel = mongoose.model("User", userSchema);
+
 
     var api = {
         createUser: createUser,
@@ -17,81 +23,44 @@ module.exports = function(app) {
     function createUser(user) {
         console.log("ENTERED ASSIGNEMNT USER MODEL");
         console.log("entered createUser method of user wc_models in server");
-        user._id = (new Date).getTime();
-        mockUsers.push(user);
-
-        return mockUsers;
-
+        return UserModel.create(user);
     };
+
+
+    //updateUser function
+    function updateUser(userId, user) {
+    return UserModel.update({_id: userId},{$set: user});
+    };
+
+    //deleteUser function
+    function deleteUser(userId) {
+      return UserModel.remove({_id: userId});
+    };
+
 
     //findAllUsers function
     function findAllUsers() {
-        console.log("ENTERED ASSIGNEMNT USER MODEL");
-        return mockUsers;
+        console.log("ENTERED ASSIGNMENT USER MODEL");
+       return UserModel.find();
     };
 
     //findUserById function
     function findUserById(userId) {
         console.log("ENTERED ASSIGNEMNT USER MODEL");
-        for (var index in mockUsers) {
-            if (mockUsers[index]._id == userId) {
-                return mockUsers[index];
-                break;
-            }
-        }
-        return null;
+        return UserModel.findById(userId);
     };
-
-    //updateUser function
-    function updateUser(userId, user) {
-        for (var index in mockUsers) {
-            if (mockUsers[index]._id == userId) {
-              mockUsers[index] = user;
-
-            }
-        }
-    };
-
-    //deleteUser function
-    function deleteUser(userId) {
-        console.log("ENTERED ASSIGNEMNT USER MODEL");
-        for (var index in mockUsers) {
-            if (mockUsers[index]._id === userId) {
-                mockUsers.splice(index, 1);
-                break;
-            }
-        }
-    };
-
-
     //findUserByUsername function
     function findUserByUsername(username) {
         console.log("ENTERED ASSIGNEMNT USER MODEL");
-        var user;
-        for (var index in mockUsers) {
-            user = mockUsers[index];
-            if (user.username === username) {
-                return user;
-                break;
-            }
-        }
-        return null;
+       return UserModel.findOne({username: username});
     };
 
     //findUserByCredentials function
     function findUserByCredentials(credentials) {
         console.log("ENTERED ASSIGNEMNT USER MODEL");
         console.log("entered in user wc_model");
-        var user
-        for (var index in mockUsers) {
-             user = mockUsers[index];
-            if (user.username === credentials.username
-                && user.password === credentials.password)
-            {
-                return user;
-                break;
-            }
-        }
-        return null;
+        var username= credentials.username;
+        var password= credentials.password;
+       return UserModel.findOne({username: username, password: password});
     };
 }

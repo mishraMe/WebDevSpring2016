cf7940ee1fd7232a4974793fac47ee06aa90e83a
mishrame <mishrame@husky.neu.edu>
2016-04-02 02:37:05 -0400

    trying to work around the stricter server at openshift

--- public/assignment/client/views/users/profile.controller.js
+++ public/assignment/client/views/users/profile.controller.js
@@ -16,38 +16,36 @@
         vm.update = update;
 
         var emails = [];
-
-        //function update(user){
-        //    vm.error = null;
-        //    vm.message = null;
-        //    console.log("user in update function is");
-        //    UserService
-        //        .updateUser(user._id, user)
-        //        .then(function (response){
-        //            console.log("response is ");
-        //            console.log(response);
-        //            vm.message = "User updated successfully"
-        //                UserService.setCurrentUser(response.data);
-        //                $location.url("/profile");
-        //            });
-        //}
-
-        var emails = [];
         emails.push(vm.currentUser.emails);
         vm.currentUser.emails=emails;
 
         function update(user){
             vm.error = null;
             vm.message = null;
+            console.log("user in update function is");
             UserService
                 .updateUser(user._id, user)
-                .then(
-                    function (response){
+                .then(function (response){
+                    console.log("response is ");
+                    console.log(response);
+                    vm.message = "User updated successfully"
                         UserService.setCurrentUser(response.data);
-                        vm.message = "User updated successfully";
                         $location.url("/profile");
                     });
         }
+
+        //function update(user){
+        //    vm.error = null;
+        //    vm.message = null;
+        //    UserService
+        //        .updateUser(user._id, user)
+        //        .then(
+        //            function (response){
+        //                UserService.setCurrentUser(response.data);
+        //                vm.message = "User updated successfully";
+        //                $location.url("/profile");
+        //            });
+        //}
 
     }
 })();

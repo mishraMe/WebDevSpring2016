4eacedd734b3ae2a2fa2ffddfdd8c81eea339703
mishrame <mishrame@husky.neu.edu>
2016-04-02 01:47:11 -0400

    trying to remove openshift's error

--- public/assignment/client/views/users/profile.controller.js
+++ public/assignment/client/views/users/profile.controller.js
@@ -16,6 +16,23 @@
         vm.update = update;
 
         var emails = [];
+
+        //function update(user){
+        //    vm.error = null;
+        //    vm.message = null;
+        //    console.log("user in update function is");
+        //    UserService
+        //        .updateUser(user._id, user)
+        //        .then(function (response){
+        //            console.log("response is ");
+        //            console.log(response);
+        //            vm.message = "User updated successfully"
+        //                UserService.setCurrentUser(response.data);
+        //                $location.url("/profile");
+        //            });
+        //}
+
+        var emails = [];
         emails.push(vm.currentUser.emails);
         vm.currentUser.emails=emails;
 
@@ -26,7 +43,7 @@
                 .updateUser(user._id, user)
                 .then(
                     function (response){
-                        UserService.setCurrentUser(vm.currentUser);
+                        UserService.setCurrentUser(response.data);
                         vm.message = "User updated successfully";
                         $location.url("/profile");
                     });

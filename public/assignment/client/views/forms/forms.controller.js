0409b2b66518d5e98d144ba79e23911e49b3b3f2
mishrame <mishrame@husky.neu.edu>
2016-04-02 08:47:07 -0400

    trying to work around the openshift mod error

--- public/assignment/client/views/forms/forms.controller.js
+++ public/assignment/client/views/forms/forms.controller.js
@@ -41,20 +41,13 @@
         }
 
         function deleteForm(form){
-            //function is responsible for deleting a form by the index value
             var formsAfterDeletion=[];
-           // console.log("form Id is");
-           // console.log(form._id);
-
             FormService
                 .deleteFormById(form._id)
                 .then(function(response){
-             //       console.log("entered then condition of deleteFormByID");
                     FormService
                         .findAllFormsForUser(vm.currentUser._id)
                         .then(function(resp){
-                          //  console.log("entrered then condition of find All forms in delete funciton");
-                          //  console.log(resp);
                             vm.forms= resp.data;
                             vm.error = null;
                         });
@@ -62,18 +55,18 @@
         }
 
         function updateForm(newForm){
-            //function is responsible for updating selected form to the new form's value
-           // console.log("entered updatedForm")
             var renewedForm = {
                 _id: newForm._id,
                 title: newForm.title,
                 userId: newForm.userId
             };
-            if(vm.form._id == null){
+            var currentFormId = vm.form._id;
+            delete vm.form._id;
+            if(currentFormId == null){
                 vm.error = "Form name cannot be empty";
             }
             FormService
-                .updateFormById(vm.form._id, renewedForm)
+                .updateFormById(currentFormId, renewedForm)
                 .then(function(response){
                     FormService
                         .findAllFormsForUser(vm.currentUser._id)
@@ -87,8 +80,6 @@
         }
 
         function selectForm($index){
-           // console.log("hello select form");
-            //function is responsible for selecting a form to edit
             vm.form = {
                 _id: vm.forms[$index]._id,
                 title: vm.forms[$index].title,

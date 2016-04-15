4028bee4aa1325b6494f0a11e2bdb7ad7afebec4
mishrame <mishrame@husky.neu.edu>
2016-04-15 17:26:03 -0400

    synching assignment css file.

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var uuid = require('node-uuid');
 // install and require the mongoose library
 var mongoose      = require('mongoose');
 
-var connectionString = 'mongodb://127.0.0.1:27017/form_maker';
-//var connectionStringProject = 'mongodb://127.0.0.1:27017/writers_club';
+//var connectionString = 'mongodb://127.0.0.1:27017/form_maker';
+var connectionStringProject = 'mongodb://127.0.0.1:27017/writers_club';
 
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
@@ -23,8 +23,8 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 }
 
 
-var db = mongoose.connect(connectionString);
-//var dbProject = mongoose.connect(connectionStringProject);
+//var db = mongoose.connect(connectionString);
+var dbProject = mongoose.connect(connectionStringProject);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,7 +42,7 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-require("./public/assignment/server/app.js")(app, db, mongoose);
-//require("./public/project/server/app.js")(app,dbProject, mongoose);
+//require("./public/assignment/server/app.js")(app, db, mongoose);
+require("./public/project/server/app.js")(app,dbProject, mongoose);
 
 app.listen(port, ipaddress);

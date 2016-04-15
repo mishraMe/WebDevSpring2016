a19fb35d5627b3a2473b4de0dffd1480d2aa70b6
mishrame <mishrame@husky.neu.edu>
2016-04-15 17:36:50 -0400

    synching my server to start openshift

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ var connectionStringProject = 'mongodb://127.0.0.1:27017/writers_club';
 
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connectionString =  process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //connectionString =  process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //    process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+    //    process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
+    //    process.env.OPENSHIFT_APP_NAME;
+
+    connectionStringProject =  process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +

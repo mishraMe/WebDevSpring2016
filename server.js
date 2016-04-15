692d048751a65a22c5d933fb3160c388fc7ba854
mishrame <mishrame@husky.neu.edu>
2016-04-15 10:23:52 -0400

    added mongoDb access to openshift

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ var mongoose      = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/form_maker';
 //var connectionStringProject = 'mongodb://127.0.0.1:27017/writers_club';
 
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connectionString =  process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+
 var db = mongoose.connect(connectionString);
 //var dbProject = mongoose.connect(connectionStringProject);
 

83cf3415fddbeb01ed6d47b1473d9656d3af8d84
mishrame <mishrame@husky.neu.edu>
2016-04-19 00:15:45 -0400

    write post, my post public post and follow verified on local, bug fixed in sidebar

--- public/project/client/views/sidebar/sidebar.controller.js
+++ public/project/client/views/sidebar/sidebar.controller.js
@@ -9,12 +9,20 @@
 
         $scope.createPost = createPost;
         var currentUser;
-        vm.currentUser = UserService.getCurrentUser()
+        var currentUserId;
         vm.$location = $location;
         console.log(vm.currentUser);
         var postTemplate;
 
         function init(){
+           UserService
+               .getCurrentUser()
+               .then(function(response){
+                   vm.currentUser = response.data;
+                   currentUser = vm.currentUser;
+                   currentUserId = vm.currentUser._id;
+                   delete vm.currentUser._id;
+               })
         }
         init();
 
@@ -30,11 +38,11 @@
                 "title": null,
                 "tag": [] ,  "type": "private",
                 "roles": ["user"],
-                "userId": currentUser._id, "username": currentUser.username,
+                "userId": currentUser.Id, "username": currentUser.username,
                 "content":null
             }
             PostService
-                .createPostForUser(currentUser._id, postTemplate)
+                .createPostForUser(currentUserId, postTemplate)
                 .then(function(response)
                 {
                     vm.post = response.data;
